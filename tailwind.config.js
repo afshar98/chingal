c57812b981df01077366786c01e0ca846dad8add
Mohammad <mo.afshar1998@gmail.com>
2023-11-03 01:17:47 +0330

    add custom colors in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        current: "currentColor",
+        transparent: "transparent",
+        primary: "#020B1F",
+        secondary: "#182040",
+        "primary-content": "#FBFDFE",
+        accent: "#0551E7",
+      }
+    },
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 }
