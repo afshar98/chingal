6b96ed444a34aa9e24bb94b32bae90a0c43b1402
Mohammad <mo.afshar1998@gmail.com>
2023-11-03 01:21:37 +0330

    create not-found component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-
+import {RouterProvider} from "react-router-dom";
+import router from './router.jsx';
 function App() {
 
   return (
     <>
-
+      <RouterProvider router={router}/>
     </>
   )
 }
