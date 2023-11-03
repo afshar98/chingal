a6272e3ddd39132da720222ed8ce7b5724bba3fe
Mohammad <mo.afshar1998@gmail.com>
2023-11-03 16:17:06 +0330

    set a condition to know if age is date type or not

--- src/helper/functions.js
+++ src/helper/functions.js
@@ -1,10 +1,16 @@
 const calculateAge = (birthdate) =>{
+    const datePattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/;
+    console.log(typeof birthdate)
     const birthDate = new Date(birthdate);
     const currentDate = new Date();
     const yearsDiff = currentDate.getFullYear() - birthDate.getFullYear();
     const monthsDiff = currentDate.getMonth() - birthDate.getMonth();
     const daysDiff = currentDate.getDate() - birthDate.getDate();
-    if (monthsDiff < 0 || (monthsDiff === 0 && daysDiff < 0)) {
+
+    if(!datePattern.test(birthdate)){
+        return birthdate;
+    }
+     else if (monthsDiff < 0 || (monthsDiff === 0 && daysDiff < 0)) {
         return yearsDiff - 1;
     } else {
         return yearsDiff;
