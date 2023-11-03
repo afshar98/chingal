461bff7e9ff3b6dfe360827077f579700bfbef7d
Mohammad <mo.afshar1998@gmail.com>
2023-11-03 19:44:17 +0330

    separate dark/light mode theme in bread-crumbs component

--- src/components/bread-crumbs.jsx
+++ src/components/bread-crumbs.jsx
@@ -8,7 +8,7 @@ function BreadCrumbs({data}) {
         <ul className="mb-8 flex flex-row gap-1">
             {
                 data.map((bread, i) =>
-                    <li key={i} className="flex flex-row items-center last-of-type:text-surface-900 text-surface-500">
+                    <li key={i} className="flex flex-row items-center last-of-type:text-surface-100 text-surface-500 dark:last-of-type:text-surface-900 dark:text-surface-500">
                         <span className="ml-1"><FiChevronLeft/></span>
                         <Link to={bread.path}>{bread.title}</Link>
                     </li>
