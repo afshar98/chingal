ffff00ca25150f009d7ff6f763613879b0e17cb6
Mohammad <mo.afshar1998@gmail.com>
2023-11-03 14:56:48 +0330

    set icon for each breadcrumb item

--- src/components/bread-crumbs.jsx
+++ src/components/bread-crumbs.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {FiChevronLeft} from "react-icons/fi";
 
 function BreadCrumbs({data}) {
 
     return (
-        <ul className="mb-8 flex flex-row gap-4">
+        <ul className="mb-8 flex flex-row gap-1">
             {
-                data.map((bread, i)=>
-                    <li key={i} className="last-of-type:text-surface-900 text-surface-500">
+                data.map((bread, i) =>
+                    <li key={i} className="flex flex-row items-center last-of-type:text-surface-900 text-surface-500">
+                        <span className="ml-1"><FiChevronLeft/></span>
                         <Link to={bread.path}>{bread.title}</Link>
                     </li>
                 )
