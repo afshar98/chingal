607f39456220932c336c0ba603f12dbfedac6b38
Mohammad <mo.afshar1998@gmail.com>
2023-11-03 19:42:02 +0330

    use theme-provider in app component to check dark/light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import {RouterProvider} from "react-router-dom";
 import router from './router.jsx';
+import {useThemeContext} from "./contexts/theme-context.jsx";
 function App() {
+    const {isDarkMode} = useThemeContext();
 
   return (
-    <>
+    <div className={isDarkMode ? 'dark' : ''}>
       <RouterProvider router={router}/>
-    </>
+    </div>
   )
 }
 
