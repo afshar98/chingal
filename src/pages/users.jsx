ccb76324d79a44aecf359e607490c026a3750200
Mohammad <mo.afshar1998@gmail.com>
2023-11-03 14:31:18 +0330

    add bread-crumbs in users component

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,13 +1,24 @@
 import UserList from "../features/users/components/user-list.jsx";
 import { httpService } from "@core/http-service";
-import { Await, defer, useLoaderData } from "react-router-dom";
+import {Await, defer, useLoaderData, useLocation} from "react-router-dom";
 import { Suspense } from "react";
+import BreadCrumbs from "../components/bread-crumbs.jsx";
 
 const Users = () => {
+
+    const location = useLocation();
+    const beadCrumbs = [
+        {
+            title: 'لیست کاربران',
+            path: location.pathname
+        }
+    ]
+
     const data = useLoaderData();
 
     return (
         <div className="p-10">
+            <BreadCrumbs data={beadCrumbs}/>
             <Suspense fallback={<p className="text-center text-lg">در حال دریافت اطلاعات ...</p>}>
                 <Await resolve={data.users}>
                     {(loadedUsers) => <UserList users={loadedUsers} />}
