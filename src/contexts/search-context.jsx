35e11bbcdb3842ef77b89801ee03d088d57560f0
Mohammad <mo.afshar1998@gmail.com>
2023-11-03 15:20:08 +0330

    create a search context

---
+++ src/contexts/search-context.jsx
@@ -0,0 +1,24 @@
+import {useContext, useState} from "react";
+import {createContext} from "react";
+
+const SearchContext = createContext();
+
+const SearchProvider = ({children}) => {
+
+    const [search, setSearch] = useState('');
+
+    const value = {
+        search,
+        setSearch,
+    }
+
+    return <SearchContext.Provider value={value}>
+        {children}
+    </SearchContext.Provider>
+}
+
+const useSearchContext = () => {
+    return useContext(SearchContext);
+}
+
+export {useSearchContext, SearchProvider}
